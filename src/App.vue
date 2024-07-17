0d4665a789dbaace4b1700a3077ee83a04cb6b2b
witan7725 <witan9876@gmail.com>
2024-07-17 11:53:21 +0700

    modified thai-lang on app.vue

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,10 @@
       <q-input
         filled
         v-model="name"
-        label="Your name *"
-        hint="Name and surname"
+        label="ชื่อของคุณ *"
+        hint="ชื่อและนามสกุล"
         lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
+        :rules="[ val => val && val.length > 0 || 'ให้พิมพ์ชื่อและนามสกุล']"
       />
 
       <q-input
